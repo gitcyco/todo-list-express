7083ce86e8c05b13368bd274b255dd7190899df7
gitcyco <gitcyco@users.noreply.github.com>
2022-07-27 14:17:34 -0700

    comment main.js, tidy up index.ejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,114 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+// main.js
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+// grab a reference to all "delete" icons (these are spans with the class "fa-trash"
+const deleteBtn = document.querySelectorAll(".fa-trash");
+// grab a reference to all spans with class "item"
+const item = document.querySelectorAll(".item span");
+// grab a reference to all spans with "item" class and "completed" class
+const itemCompleted = document.querySelectorAll(".item span.completed");
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
+// turn the deleteBtn collection into an array, and iterate it with forEach
+Array.from(deleteBtn).forEach((element) => {
+  // add 'deleteItem' function to the click event of each item
+  element.addEventListener("click", deleteItem);
+});
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
+// turn the item collection into an array, and iterate it with forEach
+Array.from(item).forEach((element) => {
+  // add 'markComplete' function to the click event of each item
+  element.addEventListener("click", markComplete);
+});
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+// turn the itemCompleted collection into an array, and iterate it with forEach
+Array.from(itemCompleted).forEach((element) => {
+  // add 'markUnComplete' function to the click event of each item
+  element.addEventListener("click", markUnComplete);
+});
 
-    }catch(err){
-        console.log(err)
-    }
+// define the deleteItem async function for removing items from the list when clicked
+async function deleteItem() {
+  // get the second child of the parentNode of this item - this selects the span, and assign the text from the span to itemText
+  const itemText = this.parentNode.childNodes[1].innerText;
+  // start try block
+  try {
+    // send a network request via fetch, to the 'deleteItem' endpoint
+    const response = await fetch("deleteItem", {
+      // HTTP method 'delete'
+      method: "delete",
+      // set basic headers
+      headers: { "Content-Type": "application/json" },
+      // send a body containing JSON of the itemText, this is the item to delete
+      body: JSON.stringify({
+        // set the property 'itemFromJS:' to 'itemText'
+        itemFromJS: itemText,
+      }),
+    });
+    // process the response stream to json data
+    const data = await response.json();
+    // log the json data
+    console.log(data);
+    // reload the window
+    location.reload();
+  } catch (err) {
+    // log any errors
+    console.log(err);
+  }
 }
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
-    }
+async function markComplete() {
+  // get the second child of the parentNode of this item - this selects the span, and assign the text from the span to itemText
+  const itemText = this.parentNode.childNodes[1].innerText;
+  // start try block
+  try {
+    // send a network request via fetch, to the 'markComplete' endpoint
+    const response = await fetch("markComplete", {
+      // HTTP method 'put'
+      method: "put",
+      // set basic headers
+      headers: { "Content-Type": "application/json" },
+      // send a body containing JSON of the itemText, this is the item to mark complete
+      body: JSON.stringify({
+        // set the property 'itemFromJS:' to 'itemText'
+        itemFromJS: itemText,
+      }),
+    });
+    // process the response stream to json data
+    const data = await response.json();
+    // log the json data
+    console.log(data);
+    // reload the window
+    location.reload();
+  } catch (err) {
+    // log any errors
+    console.log(err);
+  }
 }
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
-    }
+async function markUnComplete() {
+  // get the second child of the parentNode of this item - this selects the span, and assign the text from the span to itemText
+  const itemText = this.parentNode.childNodes[1].innerText;
+  // start try block
+  try {
+    // send a network request via fetch, to the 'markUnComplete' endpoint
+    const response = await fetch("markUnComplete", {
+      // HTTP method 'put'
+      method: "put",
+      // set basic headers
+      headers: { "Content-Type": "application/json" },
+      // send a body containing JSON of the itemText, this is the item to mark incomplete
+      body: JSON.stringify({
+        // set the property 'itemFromJS:' to 'itemText'
+        itemFromJS: itemText,
+      }),
+    });
+    // process the response stream to json data
+    const data = await response.json();
+    // log the json data
+    console.log(data);
+    // reload the window
+    location.reload();
+  } catch (err) {
+    // log any errors
+    console.log(err);
+  }
 }
